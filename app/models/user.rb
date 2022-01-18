1fab0c61c05a5b36acd92042baab86c19e1fc99e
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-01-18 15:08:52 -0800

    Created new user controller and routes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :username, length: { minimum: 2}
+    validates :username, length: { maximum: 15 }
+    validates :email, format: { with: /@/, message: "Need @"}
+    validates :password, length: { minimum: 6 }
 end
